4ee5d44f6437925be817ad4cb2a12f178cd4d900
Umpoy <iumpoy@yahoo.com>
2017-12-17 18:50:58 +0800

    added card flip

--- src/components/card.js
+++ src/components/card.js
@@ -5,28 +5,39 @@ class Card extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            zero_clicks: true,
+            clicked: false
+        }
 
         this.clicked = this.clicked.bind(this);
         // console.log(this.props.details.card)
         // console.log('CARD PROPS:', this.props);
     }
 
+
+
     clicked() {
         //console.log('Card clicked, method in card.js', this.props.details.id);
+        this.setState({
+            clicked: true
+        })
         this.props.checkCard(this.props.cardIndex);
-        console.log(this)
+
 
     }
 
     render() {
-
+        const revealed = {
+            display: 'none'
+        }
+        const set_card_clicked = this.state.clicked ? revealed : console.log('card isnt clicked');
         return (
             <div className="card" onClick={this.clicked}>
                 <div className="front">
                     <img src={this.props.details.card} />
                 </div>
-                <div className="back">
+                <div className="back" style={set_card_clicked}>
                     <img src={cardBack} />
                 </div>
             </div>
